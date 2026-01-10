0dc02b9bb334093f3b808cf13f3288b5bb3896f8
Marcos Torres <marcostor13@gmail.com>
2026-01-10 07:09:22 -0500

    Replace Node adapter with Netlify adapter in Astro configuration and update package dependencies accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
